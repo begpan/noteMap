aeec98bfa791655a51c7339519c6fa45277a0f6a
begpan <begumpandora@gmail.com>
2024-01-09 19:11:12 +0300

    ikon changed

--- helpers.js
+++ helpers.js
@@ -1,80 +1,71 @@
-// localden parametr olarak gelwn elemanı alır
+// lokalden parametre oalrak gelen elmanı alır
+export const getStorage = () => {
+  // lokalden key ile eşeleşen veriyi alma
+  const strData = localStorage.getItem('notes');
 
-export const getStorage =() =>{
-    const strData= localStorage.getItem('notes')
+  // gelen string veiryi js verisine çevir ve döndür
+  return JSON.parse(strData);
+};
 
-    // gelen string veriyi js verisibe cevir ve dondur
+// lokale parametre olarak gelen elemanı kaydeder
+export const setStorage = (data) => {
+  // stringe çevir
+  const strData = JSON.stringify(data);
 
-    return JSON.parse(strData)
-
-
-}
-
-
-// locale parametre olarak gelen elemanı kaydeder
-
-export const setStorage = (data) =>{
-
-    const strData = JSON.stringify(data)
-
-    localStorage.setItem('notes',strData)
-
-}
-
+  // lokal'e kaydet
+  localStorage.setItem('notes', strData);
+};
 
 // İKONLAR
 export var userIcon = L.icon({
-    iconUrl: '/images/Person.png',
-    iconSize: [50, 50],
-    popupAnchor: [0, -20],
-    shadowUrl: '/images/my-icon-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [30, 34],
-  });
-  
-  var homeIcon = L.icon({
-    iconUrl: '/images/Home_8.png',
-    iconSize: [70, 75],
-    popupAnchor: [0, -20],
-    shadowUrl: '/images/my-icon-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [30, 34],
-  });
-  
-  var jobIcon = L.icon({
-    iconUrl: '/images/Briefcase_8.png',
-    iconSize: [70, 75],
-    popupAnchor: [0, -20],
-    shadowUrl: '/images/my-icon-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [30, 34],
-  });
-  
-  var gotoIcon = L.icon({
-    iconUrl: '/images/Aeroplane_8.png',
-    iconSize: [70, 75],
-    popupAnchor: [0, -20],
-    shadowUrl: '/images/my-icon-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [30, 34],
-  });
-  
-  var parkIcon = L.icon({
-    iconUrl: '/images/Parking_8.png',
-    iconSize: [70, 75],
-    popupAnchor: [0, -20],
-    shadowUrl: '/images/my-icon-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [30, 34],
-  });
-  
-  //
-  export const icons = {
-    goto: gotoIcon,
-    home: homeIcon,
-    job: jobIcon,
-    park: parkIcon,
-  };
+  iconUrl: '/images/Person.png',
+  iconSize: [50, 50],
+  popupAnchor: [0, -20],
+  shadowUrl: '/images/my-icon-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [30, 34],
+});
 
-//  
+var homeIcon = L.icon({
+  iconUrl: '/images/Home_8.png',
+  iconSize: [70, 75],
+  popupAnchor: [0, -20],
+  shadowUrl: '/images/my-icon-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [30, 34],
+});
 
+var jobIcon = L.icon({
+  iconUrl: '/images/Briefcase_8.png',
+  iconSize: [70, 75],
+  popupAnchor: [0, -20],
+  shadowUrl: '/images/my-icon-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [30, 34],
+});
+
+var gotoIcon = L.icon({
+  iconUrl: '/images/Aeroplane_8.png',
+  iconSize: [70, 75],
+  popupAnchor: [0, -20],
+  shadowUrl: '/images/my-icon-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [30, 34],
+});
+
+var parkIcon = L.icon({
+  iconUrl: '/images/Parking_8.png',
+  iconSize: [70, 75],
+  popupAnchor: [0, -20],
+  shadowUrl: '/images/my-icon-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [30, 34],
+});
+
+//
+export const icons = {
+  goto: gotoIcon,
+  home: homeIcon,
+  job: jobIcon,
+  park: parkIcon,
+};
--- main.js
+++ main.js
@@ -1,201 +1,189 @@
-import { setStorage, getStorage, userIcon } from "./helpers.js";
-import { icons } from "./helpers.js";
+//!farklı dosyalar5dan gelen veriler
+import {
+  setStorage,
+  getStorage,
+  icons,
+  userIcon,
+} from './helpers.js';
 
-// html elemanlarını cagırma
-const form = document.querySelector("form");
-const noteList = document.querySelector('ul')
+//! html elemanlarını çağırma
+const form = document.querySelector('form');
+const noteList = document.querySelector('ul');
 
+//! global değişkenler (kodun heryerinden erişlebilen)
+var map;
+var coords;
+var notes = getStorage() || []; //veriler nul yerine boş dizi olsun
+var markerLayer = null;
 
-// global degiskenler
-
-let coords;
-let notes = getStorage();
-let markerLayer =[]
-let map
-
-
- 
-// haritayı ekrana basan fonksiyon
-
+//* haritayı ekran basan fonk.
 function loadMap(coords) {
-  map = L.map("map").setView(coords, 10);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  map = L.map('map').setView(coords, 15);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
-  // imleçleri tutacagımız ayrı katman olusturma
-  markerLayer= L.layerGroup().addTo(map)
+  //   imleçleri tutucağımız ayrı bir katman oluşturma
+  markerLayer = L.layerGroup().addTo(map);
 
-  // kulllanıcının konumuna imleç bas
+  // kullanıcn konumuna imleç bas
+  L.marker(coords, { icon: userIcon }).addTo(map);
 
-  L.marker(coords,{icon:userIcon}).addTo(map)
+  // lokalden gelen veileri ekrana bas
+  renderNoteList(notes);
 
-  // localden gelen verileri ekrana bas
-
-  renderNoteList(notes)
-
-  //   harştadaki tıklanma olaylarını izle
-  map.on("click", onMapClick);
+  // haritadaki tıklanma olaylarınız izle
+  map.on('click', onMapClick);
 }
 
+//* iptal butona tıklanırsa formu temizle ve kapat
+form[3].addEventListener('click', () => {
+  // formu temizle
+  form.reset();
 
+  // formu kapat
+  form.style.display = 'none';
+});
 
+//* form gönderilirse yeni bir not oluştur ve  storage'a kaydet
+form.addEventListener('submit', (e) => {
+  //1) yenilemeyi engelle
+  e.preventDefault();
 
-
-// iptal butonuna tıklanırsa formu temizle ve kapat
-
-form[3].addEventListener("click", ()=>{
-
-    // formu temizle
-    form.reset()
-    // formu kapat
-    form.style.display = "none"
-})
-
-// form gonderilirse yeni bir not olustur ve local storage a kaydet
-
-form.addEventListener('submit', (e)=> {
-    // 1) yenilemeyi engelle
-    e.preventDefault()
-
-    // 2) inputlardaki verilerden bir not objesi oluştur
-const newNote= {
+  //2) inputlardaki verilerden bir note objesi oluştur
+  const newNote = {
     id: new Date().getTime(),
     title: form[0].value,
     date: form[1].value,
     status: form[2].value,
     coords: coords,
+  };
+
+  //3) dizinin başına yeni notu ekle
+  notes.unshift(newNote);
+
+  //4) note'ları ekran bas
+  renderNoteList(notes);
+
+  //5) local storage'ı güncelle
+  setStorage(notes);
+
+  //6) formu kapat
+  form.style.display = 'none';
+  form.reset();
+});
+
+// not için imleç katmanına yeni bir imleç ekler
+function renderMarker(note) {
+  // imleç oluştur
+  L.marker(note.coords, { icon: icons[note.status] })
+    // imleci katman ekle
+    .addTo(markerLayer)
+    .bindPopup(note.title);
 }
 
-//3) dizinin başına yeni not ekleme
-notes.unshift(newNote)
+//* ekrana notları basar
+function renderNoteList(items) {
+  // önceden eklenen elemanları temizle
+  noteList.innerHTML = '';
+  markerLayer.clearLayers();
 
-// 4) notları listele
-renderNoteList(notes)
-
-// 5)local storage ı guncelle
-
-setStorage(notes)
-
-// 6)formu kapat
-
-form.style.display= 'none'
-form.reset()
-
-
-})
-
-// not için imleç katmanına bir imleç ekler
-function renderMarker(note){
-  console.log(note);
-  L.marker(note.coords,{icon: icons[note.status]})
-  // imleci katmana ekle
-  .addTo(map)
-  .bindPopup(note.title)
-}
-
-// ekrana notları basar
-
-function renderNoteList(items){
-
-// ÖNCEDEN EKLENENEN ELEMANLARI TEMİZLE
-noteList.innerHTML = '' ;
-markerLayer.clearLayers()
-
-
-    // dizideki her bir obje için not kartı bas
-
-items.forEach((note)=>{
-    // li elemanı olustur
-
-    const listEle = document.createElement('li')
+  // dizideki herbir obje için note kartı bas
+  items.forEach((note) => {
+    // li elemanı oluştur
+    const listEle = document.createElement('li');
 
     // data-id ekle
+    listEle.dataset.id = note.id;
 
-    listEle.dataset.id=note.id
-
-    // içeriği belirle
-    listEle.innerHTML = ` <div class="info">
+    // içeriğini belirle
+    listEle.innerHTML = `
+            <div class="info">
               <p>${note.title}</p>
               <p>
-                <span>Date</span>
+                <span>Tarih:</span>
                 <span>${note.date}</span>
               </p>
               <p>
-                <span>Status:</span>
+                <span>Durum:</span>
                 <span>${note.status}</span>
               </p>
             </div>
             <div class="icons">
-                <i id="fly" class="bi bi-airplane-fill"></i>
-                <i id="delete" class="bi bi-trash3-fill"></i>
-            </div>
-    `;
-
-    // elemanı haritaya ekle
-    renderMarker(note)
-
+              <i id="fly" class="bi bi-airplane-fill"></i>
+              <i id="delete" class="bi bi-trash3-fill"></i>
+            </div>    
+     `;
 
     // elemanı listeye ekle
-    noteList.appendChild(listEle)
-})
+    noteList.appendChild(listEle);
+
+    // elemanı haritay ekle
+    renderMarker(note);
+  });
 }
 
-
-
-// kullanıcının konumunu alma
+//* kullanıcının konumunu alma
 navigator.geolocation.getCurrentPosition(
+  // konumu alırsa haritayı kullanıcının konumuna göre yükler
   (e) => {
-    // konumu alırsa calısacak fonksiyon succesCallBack konuma gider
     loadMap([e.coords.latitude, e.coords.longitude]);
   },
-  // konumu alamazsa calısacak fonksiyon positionErrorCallBack :konum izni yoksa
-  //   belirlenen noktya gider
+  // konumu almazsa varsayılan olorak ankarada başlatır
   () => {
     loadMap([39.953925, 32.858552]);
   }
 );
 
-// haritadaki tıklanma olaylarında calısır
+//* haritadaki tıklanma olaylarında çalışır
+function onMapClick(event) {
+  // tıklanan yerin konumuna eriş global değişken aktardım
+  coords = [event.latlng.lat, event.latlng.lng];
 
-function onMapClick(e) {
-  // tıklanan yerin konumuna erişme
-coords = [e.latlng.lat, e.latlng.lng];
-  // haritada bir yere tıklayınca formu göster
-  form.style.display = "flex";
-  // inputun ilkine focus ekle
+  // formu göster
+  form.style.display = 'flex';
 
+  // ilk inputa odaklar
   form[0].focus();
 }
 
-// silme ve ucus
+// silme uçuş
+noteList.addEventListener('click', (e) => {
+  // tıklanılan elemanın id'sine erişme
+  const found_id = e.target.closest('li').dataset.id;
 
-noteList.addEventListener("click", (e)=>{
+  if (
+    e.target.id === 'delete' &&
+    confirm('Want to delete?')
+  ) {
+    // idsini bildiğimiz elmanı diziden çıkart
+    notes = notes.filter((note) => note.id != found_id);
 
-  // tıklanılan lemanın idsine erişme
-  const found_id = e.target.closest("li").dataset.id
-  console.log(found_id)
-
-  if(e.target.id=== 'delete' && confirm('are you sure')){
-    // idsini bildiğimizi elemanı diziden cıkar
-
-    notes = notes.filter((note) => note.id != found_id)
-
-    // lokali guncelle
-
-    setStorage(notes)
+    // lokal'i güncelle
+    setStorage(notes);
 
     // ekranı güncelle
-    renderNoteList(notes)
+    renderNoteList(notes);
   }
 
-  if(e.target.id === 'fly'){
-    // idsini bildiğimiz bir elemanı dizidwki haline erişme
+  if (e.target.id === 'fly') {
+    // id'sini bildiğimiz elmanı dizideki haline erişme
+    const note = notes.find((note) => note.id == found_id);
 
-    const note = notes.find((note)=> note.id ==found_id)
-
-    map.flyTo(note.coords)
+    // not'un kordinatlarına git
+    map.flyTo(note.coords);
   }
-})
+});
+
+
+function toggleWrapper() {
+  const wrapper = document.querySelector('.wrapper');
+  wrapper.classList.toggle('wrapper-hidden');
+}
+
+const toggleButton = document.getElementById('toggleButton');
+toggleButton.addEventListener('click', toggleWrapper);
+renderNoteList()
